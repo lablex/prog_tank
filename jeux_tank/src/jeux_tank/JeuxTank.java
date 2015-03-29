ab2ff7ba5582febfe6b6b6c42467f6bdc192aaf9
Kevin <Kevin@Kounvonglasy-Kevin>
2015-03-30 00:25:32 +0200

    Deplacement Tank

--- jeux_tank/src/jeux_tank/JeuxTank.java
+++ jeux_tank/src/jeux_tank/JeuxTank.java
@@ -2,10 +2,11 @@ package jeux_tank;
 
 public class JeuxTank {
 
-	public static void main(String[] args) {
-		Fenetre fen = new Fenetre();
-		fen.setVisible(true);
-
-	}
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        tank.deplacement_rotation();
+        //Fenetre fen = new Fenetre();
+        //fen.setVisible(true); 
+    }
 
 }
