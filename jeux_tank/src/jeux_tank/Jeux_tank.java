5070db0e2868a75b7aa302e223c9a79470031b18
Kevin <Kevin@Kounvonglasy-Kevin>
2015-04-05 11:08:23 +0200

    Ajout de la classe Jeux_tank

---
+++ jeux_tank/src/jeux_tank/Jeux_tank.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeux_tank;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Jeux_tank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Fenetre fen = new Fenetre();
+        fen.setVisible(true); 
+    }
+    
+}
