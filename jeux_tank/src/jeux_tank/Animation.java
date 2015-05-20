3412651cc8ccd0f7d1dc07bc7ddc59e3ae20d3ec
Kevin <Kevin@Kounvonglasy-Kevin>
2015-05-20 20:41:47 +0200

    Modifications: Ajout du timer (WIP)

--- jeux_tank/src/jeux_tank/Animation.java
+++ jeux_tank/src/jeux_tank/Animation.java
@@ -7,143 +7,150 @@ package jeux_tank;
 
 import javax.swing.JPanel;
 
-public class Animation  extends Thread {
-	private volatile boolean avance= false;
-	private volatile boolean tir=false;
-	private volatile boolean altern;
-	private JPanel pan;
-	private Missile missile1;
-	private Missile missile2;
-	private Tank tank1;
-	private Tank tank2;
-	private int count;
-        
-	public Animation(Missile missile1, Missile missile2, Tank tank1, Tank tank2, JPanel pan) {
-		this.missile1 = missile1;
-		this.missile2 = missile2;
-		this.tank1 = tank1;
-		this.tank2 = tank2;
-		this.pan=pan;
-		
-		this.tank1.setPointsTank();
-		this.tank1.setPositionTankX();
-		this.tank1.setPositionTankY();
-		this.tank1.setPositionCanonX();
-		this.tank1.setPositionCanonY();
-		this.tank1.setAngleTank();
-		
-		this.tank2.setPointsTank();
-		this.tank2.setPositionTankX();
-		this.tank2.setPositionTankY();
-		this.tank2.setPositionCanonX();
-		this.tank2.setPositionCanonY();
-		this.tank2.setAngleTank();
-		
-		this.tank1.setPointsTank();
-		this.tank1.setPositionTankX();
-		this.tank1.setPositionTankY();
-		this.tank1.setPositionCanonX();
-		this.tank1.setPositionCanonY();
-		this.tank1.setAngleTank();
-		
-		this.tank2.setPointsTank();
-		this.tank2.setPositionTankX();
-		this.tank2.setPositionTankY();
-		this.tank2.setPositionCanonX();
-		this.tank2.setPositionCanonY();
-		this.tank2.setAngleTank();
-	}
+public class Animation extends Thread {
 
-	public void run() {
+    private volatile boolean avance = false;
+    private volatile boolean tir = false;
+    private volatile boolean altern;
+    private JPanel pan;
+    private Missile missile1;
+    private Missile missile2;
+    private Tank tank1;
+    private Tank tank2;
+    private static double count;
 
-		while (true) {
-				try {
-					
-					count++;
-					if(count<10000){
-						
-						altern=false;
-						if(count==9999){
-							avance= false;
-							tir=false;
-							Fenetre.setAvance(true);
-							Fenetre.setTir(true);
-						}
-					}else if(10000<=count && count<20000){
-						altern=true;
-					}else{
-						count=0;
-						avance= false;
-						tir=false;
-						Fenetre.setAvance(true);
-						Fenetre.setTir(true);
-					}
-					
-					
-					if(altern){
-						if(tir && !avance){
-							missile1.setMissile(Missile.getV0());
-							missile1.setPosition();
-							if(!missile1.getRunning() && missile1.getI() !=0){
-								missile1.setRunning(true);
-							}
-						}
-						if(!tir && avance){
-							tank1.setPointsTank();
-							tank1.setPositionTankX();
-							tank1.setPositionTankY();
-							tank1.setPositionCanonX();
-							tank1.setPositionCanonY();
-							tank1.setAngleTank();
-						}
-						
-					}else{
-						if(tir && !avance){
-							missile2.setMissile(Missile.getV0());
-							missile2.setPosition();
-							if(!missile2.getRunning() && missile2.getI() != 0){
-								missile2.setRunning(true);
-							}
-						}if(!tir && avance){
-							tank2.setPointsTank();
-							tank2.setPositionTankX();
-							tank2.setPositionTankY();
-							tank2.setPositionCanonX();
-							tank2.setPositionCanonY();
-							tank2.setAngleTank();
-						}
-					}
-					
-					pan.repaint();
-					Thread.sleep(1);
-					
-				} catch (InterruptedException e) {
-					Thread.currentThread().interrupt(); // Très important de réinterrompre
-					break; // Sortie de la boucle infinie
+    public Animation(Missile missile1, Missile missile2, Tank tank1, Tank tank2, JPanel pan) {
+        altern = false;
+        this.missile1 = missile1;
+        this.missile2 = missile2;
+        this.tank1 = tank1;
+        this.tank2 = tank2;
+        this.pan = pan;
 
-				}
-		}
+        this.tank1.setPointsTank();
+        this.tank1.setPositionTankX();
+        this.tank1.setPositionTankY();
+        this.tank1.setPositionCanonX();
+        this.tank1.setPositionCanonY();
+        this.tank1.setAngleTank();
 
-	}
-	
-	public boolean getAltern(){
-		return altern;
-	}
-	
-	public boolean getAvance(){
-		return avance;
-	}
-	
-	public boolean getTir(){
-		return tir;
-	}
-	
-	public void setAvance(boolean avance){
-		this.avance=avance;
-	}
-	
-	public void setTir(boolean tir){
-		this.tir= tir;
-	}
-        
+        this.tank2.setPointsTank();
+        this.tank2.setPositionTankX();
+        this.tank2.setPositionTankY();
+        this.tank2.setPositionCanonX();
+        this.tank2.setPositionCanonY();
+        this.tank2.setAngleTank();
+
+        this.tank1.setPointsTank();
+        this.tank1.setPositionTankX();
+        this.tank1.setPositionTankY();
+        this.tank1.setPositionCanonX();
+        this.tank1.setPositionCanonY();
+        this.tank1.setAngleTank();
+
+        this.tank2.setPointsTank();
+        this.tank2.setPositionTankX();
+        this.tank2.setPositionTankY();
+        this.tank2.setPositionCanonX();
+        this.tank2.setPositionCanonY();
+        this.tank2.setAngleTank();
+    }
+
+    public void run() {
+
+        while (true) {
+            try {
+                count++;
+                if (count < 10000) {
+                    if (count == 9999) {
+                        avance = false;
+                        tir = false;
+                        Fenetre.setAvance(true);
+                        Fenetre.setTir(true);
+                    }
+                } else {
+                    if (altern) {
+                        altern = false;
+                    } else {
+                        altern = true;
+                    }
+                    count = 0;
+                    avance = false;
+                    tir = false;
+                    Fenetre.setAvance(true);
+                    Fenetre.setTir(true);
+                }
+
+                if (altern) {
+                    if (tir && !avance) {
+                        missile1.setMissile(Missile.getV0());
+                        missile1.setPosition();
+                        if (!missile1.getRunning() && missile1.getI() != 0) {
+                            missile1.setRunning(true);
+                        }
+                    }
+                    if (!tir && avance) {
+                        tank1.setPointsTank();
+                        tank1.setPositionTankX();
+                        tank1.setPositionTankY();
+                        tank1.setPositionCanonX();
+                        tank1.setPositionCanonY();
+                        tank1.setAngleTank();
+                    }
+
+                } else {
+                    if (tir && !avance) {
+                        missile2.setMissile(Missile.getV0());
+                        missile2.setPosition();
+                        if (!missile2.getRunning() && missile2.getI() != 0) {
+                            missile2.setRunning(true);
+                        }
+                    }
+                    if (!tir && avance) {
+                        tank2.setPointsTank();
+                        tank2.setPositionTankX();
+                        tank2.setPositionTankY();
+                        tank2.setPositionCanonX();
+                        tank2.setPositionCanonY();
+                        tank2.setAngleTank();
+                    }
+                }
+
+                pan.repaint();
+                Thread.sleep(1);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt(); // Très important de réinterrompre
+                break; // Sortie de la boucle infinie
+
+            }
+        }
+
+    }
+
+    public boolean getAltern() {
+        return altern;
+    }
+
+    public boolean getAvance() {
+        return avance;
+    }
+
+    public boolean getTir() {
+        return tir;
+    }
+
+    public void setAvance(boolean avance) {
+        this.avance = avance;
+    }
+
+    public void setTir(boolean tir) {
+        this.tir = tir;
+    }
+
+    public static String getTime() {
+        String time = String.valueOf(Math.round(count / 1000 * 1000) / 1000);
+        return time;
+    }
+
 }
--- jeux_tank/src/jeux_tank/Draw.java
+++ jeux_tank/src/jeux_tank/Draw.java
@@ -6,6 +6,7 @@
 package jeux_tank;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 
@@ -16,90 +17,96 @@ import javax.swing.JPanel;
  *
  * @author Kevin
  */
-public class Draw extends JPanel{
+public class Draw extends JPanel {
 
-	private final int taille_image = 50;
-	private final int taille_image2 = 100;
+    private final int taille_image = 50;
+    private final int taille_image2 = 100;
 
-	protected Terrain terrain;
-	protected Tank[] tank;
-	protected Point[] pointTerrain;
-	protected Missile missile1;
-	protected Missile missile2;
+    protected Terrain terrain;
+    protected Tank[] tank;
+    protected Point[] pointTerrain;
+    protected Missile missile1;
+    protected Missile missile2;
 
-	public Draw(Tank[] tank, int n, Terrain terrain, Missile missile1, Missile missile2) {
-		this.tank = new Tank[n];
-		this.missile1 = missile1;
-		this.missile2 = missile2;
-		this.terrain = terrain;
+    public Draw(Tank[] tank, int n, Terrain terrain, Missile missile1, Missile missile2) {
+        this.tank = new Tank[n];
+        this.missile1 = missile1;
+        this.missile2 = missile2;
+        this.terrain = terrain;
 
-		for (int i = 0; i < tank.length; i++) {
+        for (int i = 0; i < tank.length; i++) {
 
-			this.tank[i] = tank[i];
-		}
-	}
+            this.tank[i] = tank[i];
+        }
+    }
 
-	public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
 
-		g.translate(0, 400);
-		drawTerrain(g, terrain.getTerrainX(), terrain.getTerrainY());
-		drawCiel(g, terrain.getCielX(), terrain.getCielY());
-		g.setColor(Color.RED);
-		drawTrajectoir(g, missile2.getPosition(), "missile2");
-		drawTrajectoir(g, missile1.getPosition(), "missile1");
-		for (int i = 0; i < tank.length; i++) {
-			drawTank(g, tank[i].getPositionTankX(),tank[i].getPositionTankY(),tank[i]);
-		}
-	}
+        g.translate(0, 400);
+        drawTerrain(g, terrain.getTerrainX(), terrain.getTerrainY());
+        drawCiel(g, terrain.getCielX(), terrain.getCielY());
+        g.setColor(Color.RED);
+        drawTimer(g);
+        drawTrajectoir(g, missile2.getPosition(), "missile2");
+        drawTrajectoir(g, missile1.getPosition(), "missile1");
+        for (int i = 0; i < tank.length; i++) {
+            drawTank(g, tank[i].getPositionTankX(), tank[i].getPositionTankY(), tank[i]);
+        }
+    }
 
-	public void drawTrajectoir(Graphics g, Point point, String name) {
-		g.drawOval((int) point.getPointX(), -(int) point.getPointY(), 25, 25);
+    public void drawTrajectoir(Graphics g, Point point, String name) {
+        g.drawOval((int) point.getPointX(), -(int) point.getPointY(), 25, 25);
 
-	}
-	
-	public void repainting() {
-		repaint();
-	}
+    }
 
-	public void drawTank(Graphics g, int x, int y, Tank tank) {
+    public void repainting() {
+        repaint();
+    }
 
-		// Dessin Tank
-		g.drawImage(tank.rotationImageTank(tank.getImTank(), tank.getAngleTank()), x, y,taille_image, taille_image, this);
+    public void drawTimer(Graphics g) {
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+        g.drawString(Animation.getTime(), 500, -350);
+    }
 
-		// Dessin Canon
-		g.drawImage(
-				tank.rotationImageTank(tank.rotationImageCanon(tank.getImCanon(), tank.getAngleCanon()),tank.getAngleTank()),tank.getPositionCanonX(),tank.getPositionCanonY(),taille_image2, taille_image2, this);
-	}
+    public void drawTank(Graphics g, int x, int y, Tank tank) {
 
-	public void drawTerrain(Graphics g, int[] terrainX, int[] terrainY) {
+        // Dessin Tank
+        g.drawImage(tank.rotationImageTank(tank.getImTank(), tank.getAngleTank()), x, y, taille_image, taille_image, this);
 
-		int[] X1 = new int[Terrain.getNbPoint()];
-		int[] Y1 = new int[Terrain.getNbPoint()];
-		pointTerrain = terrain.getTab();
+        // Dessin Canon
+        g.drawImage(
+                tank.rotationImageTank(tank.rotationImageCanon(tank.getImCanon(), tank.getAngleCanon()), tank.getAngleTank()), tank.getPositionCanonX(), tank.getPositionCanonY(), taille_image2, taille_image2, this);
+    }
 
-		for (int i = 0; i < Terrain.getNbPoint(); i++) {
-			X1[i] = (int) pointTerrain[i].getPointX();
-			Y1[i] = (int) pointTerrain[i].getPointY();
-		}
+    public void drawTerrain(Graphics g, int[] terrainX, int[] terrainY) {
 
-		g.setColor(Color.getHSBColor(121, 248, 248));
-		g.fillPolygon(terrainX, terrainY, Terrain.getNbPoint());
+        int[] X1 = new int[Terrain.getNbPoint()];
+        int[] Y1 = new int[Terrain.getNbPoint()];
+        pointTerrain = terrain.getTab();
 
-	}
+        for (int i = 0; i < Terrain.getNbPoint(); i++) {
+            X1[i] = (int) pointTerrain[i].getPointX();
+            Y1[i] = (int) pointTerrain[i].getPointY();
+        }
 
-	public void drawCiel(Graphics g, int[] cielX, int[] cielY) {
+        g.setColor(Color.getHSBColor(121, 248, 248));
+        g.fillPolygon(terrainX, terrainY, Terrain.getNbPoint());
 
-		int[] X1 = new int[Terrain.getNbPoint()];
-		int[] Y1 = new int[Terrain.getNbPoint()];
-		pointTerrain = terrain.getTab();
+    }
 
-		for (int i = 0; i < Terrain.getNbPoint(); i++) {
-			X1[i] = (int) pointTerrain[i].getPointX();
-			Y1[i] = (int) pointTerrain[i].getPointY();
-		}
+    public void drawCiel(Graphics g, int[] cielX, int[] cielY) {
 
-		g.setColor(Color.CYAN);
-		g.fillPolygon(cielX, cielY, Terrain.getNbPoint());
-	}
+        int[] X1 = new int[Terrain.getNbPoint()];
+        int[] Y1 = new int[Terrain.getNbPoint()];
+        pointTerrain = terrain.getTab();
+
+        for (int i = 0; i < Terrain.getNbPoint(); i++) {
+            X1[i] = (int) pointTerrain[i].getPointX();
+            Y1[i] = (int) pointTerrain[i].getPointY();
+        }
+
+        g.setColor(Color.CYAN);
+        g.fillPolygon(cielX, cielY, Terrain.getNbPoint());
+    }
 
 }
